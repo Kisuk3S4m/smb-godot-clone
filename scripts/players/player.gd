2c49113785208c7db7de2a115e9df273f547d736
Kisuk3S4m <homelessorpenguin@gmail.com>
2025-10-18 19:20:56 +0200

    Play jump animation while jumping and on air

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -30,7 +30,10 @@ func update_animation() -> void:
     if direction != 0:
         animated_sprite.flip_h = direction < 0
 
-    if velocity.x != 0:
-        animated_sprite.play("run")
+    if velocity.y != 0:
+        animated_sprite.play("jump")
     else:
-        animated_sprite.play("idle")
+        if velocity.x != 0:
+            animated_sprite.play("run")
+        else:
+            animated_sprite.play("idle")
