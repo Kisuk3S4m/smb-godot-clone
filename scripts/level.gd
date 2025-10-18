d09e22e0f6b3c87f7a4199b94faa8d3a85e54e06
Kisuk3S4m <homelessorpenguin@gmail.com>
2025-10-18 20:17:59 +0200

    Improve camera limits calculation
    Compare the background width and right bottom border
    of the ground tilemap and use the larger of the two to set
    the camera right and bottom limits. This prevents the camera
    from going out of bounds when the ground tilemap is smaller
    than the background.

--- scripts/level.gd
+++ scripts/level.gd
@@ -6,18 +6,21 @@ extends Node2D
 @onready var player_camera: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
-    add_to_group("level")
-    set_camera_limits()
+	add_to_group("level")
+	set_camera_limits()
 
 func set_camera_limits() -> void:
-    var background_sprite: Sprite2D = background.get_child(0) as Sprite2D
-    var background_size: Vector2 = background_sprite.texture.get_size() * background_sprite.scale
-    
-    var right_bottom: Vector2 = ground.get_used_rect().end
-    var tile_size: Vector2 = ground.tile_set.tile_size
+	var background_sprite: Sprite2D = background.get_child(0) as Sprite2D
+	var background_size: Vector2 = background_sprite.texture.get_size() * background_sprite.scale
 
-    player_camera.limit_top = 0
-    player_camera.limit_left = 0
+	var right_bottom: Vector2 = ground.get_used_rect().end
+	var tile_size: Vector2 = ground.tile_set.tile_size
 
-    player_camera.limit_bottom = int(right_bottom.y * tile_size.y)
-    player_camera.limit_right = int(background_size.x)
+	player_camera.limit_top = 0
+	player_camera.limit_left = 0
+
+	var max_x: float = max(background_size.x, right_bottom.x * tile_size.x)
+	var max_y: float = max(background_size.y, right_bottom.y * tile_size.y)
+
+	player_camera.limit_bottom = int(max_y)
+	player_camera.limit_right = int(max_x)
