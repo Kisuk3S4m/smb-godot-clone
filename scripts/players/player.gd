56de5ed3cc0973ccc47aa8527bb86876a1e55621
Kisuk3S4m <homelessorpenguin@gmail.com>
2025-10-18 15:26:39 +0200

    Apply gravity to player velocity when not on floor
    Apply gravity to player velocity when not on floor
    with default gravity value of 980 pixels per second
    squared.

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -3,7 +3,10 @@ extends CharacterBody2D
 const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+    if not is_on_floor():
+        velocity += get_gravity() * delta
+
     if Input.is_action_just_pressed("jump") and is_on_floor():
         velocity.y = JUMP_VELOCITY
 
