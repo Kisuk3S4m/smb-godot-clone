5e1195478d7b54aed495be96ff3bcc22aefc8df7
Kisuk3S4m <homelessorpenguin@gmail.com>
2025-10-18 20:11:39 +0200

    Set camera bottom limit based on level bottom border

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var background: Parallax2D = $Background
+@onready var ground: TileMapLayer = $Ground
+
 @onready var player_camera: Camera2D = $Player/Camera2D
 
 func _ready() -> void:
@@ -11,8 +13,11 @@ func set_camera_limits() -> void:
     var background_sprite: Sprite2D = background.get_child(0) as Sprite2D
     var background_size: Vector2 = background_sprite.texture.get_size() * background_sprite.scale
     
+    var right_bottom: Vector2 = ground.get_used_rect().end
+    var tile_size: Vector2 = ground.tile_set.tile_size
+
     player_camera.limit_top = 0
     player_camera.limit_left = 0
 
-    player_camera.limit_bottom = int(background_size.y)
+    player_camera.limit_bottom = int(right_bottom.y * tile_size.y)
     player_camera.limit_right = int(background_size.x)
