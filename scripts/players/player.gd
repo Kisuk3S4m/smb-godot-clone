06785102f86b9b1ffb7db74cbda780df89eee51f
Kisuk3S4m <homelessorpenguin@gmail.com>
2025-10-18 18:58:38 +0200

    Refactor movement logic to separate update_movement function

--- scripts/players/player.gd
+++ scripts/players/player.gd
@@ -4,6 +4,9 @@ const SPEED = 200.0
 const JUMP_VELOCITY = -350.0
 
 func _physics_process(delta: float) -> void:
+    update_movement(delta)
+
+func update_movement(delta: float) -> void:
     if not is_on_floor():
         velocity += get_gravity() * delta
 
